12c43a9e4c176cecfd9390a94b8bae26196fbb86
Wesley Urban <wes.urb@gmail.com>
2019-11-07 15:50:25 +0100

    formatting

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,5 @@
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
-
 const messageOne = document.querySelector('#messageOne')
 const messageTwo = document.querySelector('#messageTwo')
 
